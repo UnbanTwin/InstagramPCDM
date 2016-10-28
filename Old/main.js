94e0e6146306d7c3280227658c4f78198a7400e9
sam bolton <info@sambolton.me>
2016-10-28 12:07:10 +0100

    Remove password

--- Old/main.js
+++ Old/main.js
@@ -1,37 +1,45 @@
 var Client = require('instagram-private-api').V1;
 var device = new Client.Device('wizardsambolton');
-var storage = new Client.CookieFileStorage(__dirname + '/cookies/wizardsambolton.json');
-
+var storage = new Client.CookieFileStorage(__dirname + '/cookies/human11404.json');
+var _ = require('underscore');
+var Promise = require('bluebird');
 // And go for login
-Client.Session.create(device, storage, 'wizardsambolton', 'sam66sam')
+Client.Session.create(device, storage, 'wzrdsmbltn', 'Sam66sam')
 .then(function(session) {
-	// Now you have a session, we can follow / unfollow, anything...
-	// And we want to follow Instagram official profile
-	return [session, Client.Account.searchForUser(session, 'instagram')]
-})
+
+	/*var feed = new Client.Feed.Inbox(session, 100);
+	feed.all().then(function(threads) {
+	    if (threads.length > 0)
+	        throw new Error("..... no messeges in inbox ....")
+	    return threads[0].configureText("text to someone")
+	})
+	.then(function(){
+	    console.log("text sended!")
+	})*/
+});
 
 function sendDM(user, text) {
-    // Either gain already gained session
-    var session = new Client.Session(device, storage);
-    // Or if we cant, create a new session
-    var promise = Client.Session.create(device, storage, 'wizardsambolton', 'sam66sam');
+	// Either gain already gained session
+	var session = new Client.Session(device, storage);
+	// Or if we cant, create a new session
+	var promise = Client.Session.create(device, storage, 'human11404', '');
 
-    promise.then(function(sessionInstance) {
-        // Search for the User
-        Client.Account.searchForUser(session, user)
-         .then(function(accountInstance) {
-            var userId = accountInstance.id;
+	promise.then(function(sessionInstance) {
+		// Search for the User
+		Client.Account.searchForUser(session, user)
+		.then(function(accountInstance) {
+			var userId = accountInstance.id;
 
-            // Send a DM
-            Client.Thread.configureText(session, userId, text)
-             .then(function(threads) {
-                var thread = threads[0];
-                //thread.broadcastText(text);
-                console.log(thread.items) // -> see conversation
-            });
-         });
-    });
+			// Send a DM
+			Client.Thread.configureText(session, userId, text)
+			.then(function(threads) {
+				var thread = threads[0];
+				//thread.broadcastText(text);
+				console.log(thread.items)
+			});
+		});
+	});
 }
 
 
-sendDM("mar.fmf","test");
+sendDM("wzrdsmbltn","test");
