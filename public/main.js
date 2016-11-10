6fb57ed5430012c477c80aef64203e0a42a7b79d
sam bolton <info@sambolton.me>
2016-11-10 19:57:37 +0000

    add users selection for messages

--- public/main.js
+++ public/main.js
@@ -1,10 +1,15 @@
 $(main);
+var toUsers = [];
+
 function main() {
+
 	$.get("/api/list/following",function(followers){
 		console.log(followers);
 		$.get('/templates/list-following.mu', function(template) {
 			var rendered = Mustache.render(template, {data: followers});
 			$('#left').html(rendered);
+			onReady();
+
 		});
 	});
 	$("#send-message-form").submit(function(){
@@ -12,7 +17,7 @@ function main() {
 		console.log("sent!");
 	});*/
 	var message = $("#send-message-form input").val();
-	$.post( "/api/send/message", { users:"3088383945",message:message })
+	$.post("/api/send/message", { users:toUsers.join(","),message:message })
 	.done(function(data) {
 		console.log("It works!");
 	});
@@ -20,4 +25,16 @@ function main() {
 	return false;
 });
 
+};
+function onReady() {
+	$(".account").click(function(){
+		var testID = $(this).data("id");
+		if(toUsers.indexOf(testID) == -1) {
+			toUsers.push(testID);
+		}else {
+			console.log("Duplicate ID");
+		}
+
+		$("#to-users").html(toUsers.join(" "));
+	});
 }
