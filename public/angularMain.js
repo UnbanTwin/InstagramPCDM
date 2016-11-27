67bd7e4b289805a255d271c087a4abfbddeb9be0
sam bolton <info@sambolton.me>
2016-11-27 14:54:50 +0000

    start service stuff

--- public/angularMain.js
+++ public/angularMain.js
@@ -1,4 +1,7 @@
 var app = angular.module("pcdm",[]);
+app.factory("info",function(){
+	return {x: 5};
+});
 app.controller("friendsList",function($scope,$http){
 	$scope.data = [];
 	$http.get("/api/list/following").then(function(res){
@@ -6,10 +9,30 @@ app.controller("friendsList",function($scope,$http){
 	});
 
 });
-app.controller("threadsList",function($scope,$http){
+app.controller("threadsList",["$scope","$http","info",function($scope,$http,info){
 	$scope.data = [];
+
+	$scope.x = info.x;
+	$scope.$watch("info.x", function(){
+		console.log("Threads list updating")
+	});
 	$http.get("/api/list/threads").then(function(res){
+		$scope.data = res.data;
+
+	});
+}]);
+app.controller("threadView",function($scope,$http){
+	$scope.data = [];
+	$http.get("/api/show/thread/340282366841710300949128113874027408665").then(function(res){
 		$scope.data = res.data;
 	});
 });
+app.controller("testCom",["$scope","info",function($scope,info){
+	$scope.data = [];
+	$scope.update = function() {
+		console.log("Updated x value");
+		info.x++;
+	}
+
+}]);
 console.log("Hello World!");
